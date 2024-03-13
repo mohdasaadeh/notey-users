cc29d7dbbb98af9466e35c79f5235e0c0a02bea9
mohdasaadeh <m.saadeh@wajeez.co>
2024-03-13 13:56:07 +0300

    creating the service routes and cli

---
+++ user-server.mjs
@@ -0,0 +1,255 @@
+import restify from "restify";
+import * as util from "util";
+import DBG from "debug";
+
+import {
+  SQUser,
+  connectDB,
+  userParams,
+  findOneUser,
+  createUser,
+  sanitizedUser,
+} from "./users-sequelize.mjs";
+
+const log = DBG("users:service");
+const error = DBG("users:error");
+
+const server = restify.createServer({
+  name: "User-Auth-Service",
+  version: "0.0.1",
+});
+
+server.use(restify.plugins.authorizationParser());
+server.use(check);
+server.use(restify.plugins.queryParser());
+server.use(
+  restify.plugins.bodyParser({
+    mapParams: true,
+  })
+);
+
+server.post("/create-user", (req, res, next) => {
+  connectDB()
+    .then(() => {
+      createUser(req).then((result) => {
+        res.contentType = "json";
+        res.send(result);
+
+        next(false);
+      });
+    })
+    .catch((error) => {
+      res.send(500, error);
+
+      next(false);
+    });
+});
+
+server.post("/find-or-create", (req, res, next) => {
+  connectDB()
+    .then(() => {
+      findOneUser(req.params.username).then((user) => {
+        if (!user) {
+          createUser(req).then((user) => {
+            if (!user) {
+              throw new Error("No user created");
+            }
+          });
+        }
+
+        res.contentType = "json";
+        res.send(user);
+
+        return next(false);
+      });
+    })
+    .catch((error) => {
+      res.send(500, error);
+
+      next(false);
+    });
+});
+
+server.get("/find/:username", (req, res, next) => {
+  connectDB()
+    .then(() => {
+      findOneUser(req.params.username).then((user) => {
+        if (!user) {
+          res.send(404, new Error("Did not find " + req.params.username));
+        } else {
+          res.contentType = "json";
+          res.send(user);
+        }
+        next(false);
+      });
+    })
+    .catch((error) => {
+      res.send(500, error);
+
+      next(false);
+    });
+});
+
+server.get("/list", (req, res, next) => {
+  connectDB()
+    .then(() => {
+      SQUser.findAll({}).then((userList) => {
+        let userListMapped = userList.map((u) => sanitizedUser(u));
+
+        if (!userList) {
+          userListMapped = [];
+        }
+
+        res.contentType = "json";
+        res.send(userListMapped);
+
+        next(false);
+      });
+    })
+    .catch((error) => {
+      res.send(500, error);
+
+      next(false);
+    });
+});
+
+server.post("/update-user/:username", (req, res, next) => {
+  connectDB()
+    .then(() => {
+      const toupdate = userParams(req);
+
+      SQUser.update(toupdate, {
+        where: { username: req.params.username },
+      }).then(() => {
+        findOneUser(req.params.username).then((result) => {
+          console.log(result);
+
+          res.contentType = "json";
+          res.send(result);
+
+          next(false);
+        });
+      });
+    })
+    .catch((error) => {
+      res.send(500, error);
+
+      next(false);
+    });
+});
+
+server.del("/destroy/:username", (req, res, next) => {
+  connectDB()
+    .then(() => {
+      SQUser.findOne({
+        where: { username: req.params.username },
+      }).then((user) => {
+        if (!user) {
+          res.send(
+            404,
+            new Error(`Did not find requested ${req.params.username} to delete`)
+          );
+        } else {
+          user.destroy();
+
+          res.contentType = "json";
+          res.send({});
+        }
+
+        next(false);
+      });
+    })
+    .catch((error) => {
+      res.send(500, error);
+      next(false);
+    });
+});
+
+server.post("/password-check", (req, res, next) => {
+  connectDB()
+    .then(() => {
+      SQUser.findOne({
+        where: { username: req.params.username },
+      }).then((user) => {
+        let checked;
+
+        if (!user) {
+          checked = {
+            check: false,
+            username: req.params.username,
+            message: "Could not find user",
+          };
+        } else if (
+          user.username === req.params.username &&
+          user.password === req.params.password
+        ) {
+          checked = { check: true, username: user.username };
+        } else {
+          checked = {
+            check: false,
+            username: req.params.username,
+            message: "Incorrect password",
+          };
+        }
+
+        res.contentType = "json";
+        res.send(checked);
+
+        next(false);
+      });
+    })
+    .catch((error) => {
+      res.send(500, error);
+
+      next(false);
+    });
+});
+
+server.listen(process.env.PORT, "localhost", function () {
+  log(server.name + " listening at " + server.url);
+});
+
+process.on("uncaughtException", function (err) {
+  console.error("UNCAUGHT EXCEPTION - " + (err.stack || err));
+
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (reason, p) => {
+  console.error(`UNHANDLED PROMISE REJECTION: ${util.inspect(p)}
+reason: ${reason}`);
+
+  process.exit(1);
+});
+
+// Mimic API Key authentication.
+var apiKeys = [{ user: "them", key: "D4ED43C0-8BD6-4FE2-B358-7C0E230D11EF" }];
+
+function check(req, res, next) {
+  if (req.authorization && req.authorization.basic) {
+    var found = false;
+
+    for (let auth of apiKeys) {
+      if (
+        auth.key === req.authorization.basic.password &&
+        auth.user === req.authorization.basic.username
+      ) {
+        found = true;
+
+        break;
+      }
+    }
+
+    if (found) {
+      next();
+    } else {
+      res.send(401, new Error("Not authenticated"));
+
+      next(false);
+    }
+  } else {
+    res.send(500, new Error("No Authorization Key"));
+
+    next(false);
+  }
+}
